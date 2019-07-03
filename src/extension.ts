2856a236a38fb8818267050678036e74323c50ba
Your Name <danyapomfyuk@gmail.com>
2019-07-03 21:12:57 +0300

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "vscode-mikero-tools-integration" is now active!');
+
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
