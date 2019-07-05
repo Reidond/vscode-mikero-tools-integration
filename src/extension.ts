5c7f5311c915c61383b410d350b50e11ae620ca2
Reidond <reidond@gmail.com>
2019-07-05 22:42:23 +0300

    Refactor powershell command runner

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import Shell from 'node-powershell';
 
 interface Command {
 	program: string | undefined;
@@ -13,12 +14,26 @@ enum TypeOfCommand {
 	Rapify,
 	Default
 }
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "vscode-mikero-tools-integration" is now active!');
 
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
+function runMikeroToolsFromPowershell(commandObj: Command) {
+	const ps = new Shell({
+		executionPolicy: 'Bypass',
+		noProfile: true
 	});
+	ps.addCommand(`& "${commandObj.program}" ${commandObj.options} ${commandObj.filePath}`);
+	ps
+		.invoke()
+		.then((output: any) => {
+			vscode.window.showInformationMessage(output);
+		})
+		.catch((err: any) => {
+			vscode.window.showInformationMessage(err);
+			ps.dispose();
+	});
+}
 
 	context.subscriptions.push(disposable);
 }
